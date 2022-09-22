4b9c888d064c9d3a882760673f9b50067f64b7b4
KIMERA ERIASAF <eriasaf31@gmail.com>
2022-09-22 22:39:07 +0300

    pages

---
+++ app/lib/farmer.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Farmer {
+  String name;
+  String phonenumber;
+  String address;
+  double rate;
+  String status;
+  double ratings;
+  AssetImage profilepic;
+  String occupation;
+  Farmer(this.name, this.phonenumber, this.address, this.rate, this.ratings,
+      this.status, this.profilepic, this.occupation);
+}
