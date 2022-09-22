fc8335894de0a3351dd52f961318153102fe3f3e
KIMERA ERIASAF <eriasaf31@gmail.com>
2022-09-22 21:56:59 +0300

    main

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,40 +1,122 @@
+// ignore: unused_import
+import 'dart:async';
+import 'package:app/farmer.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.green),
-      home: const RootPage(),
+      title: 'google Maps Demo',
+      home: MapSample(),
     );
   }
 }
 
-class RootPage extends StatefulWidget {
-  const RootPage({Key? key}) : super(key: key);
-
+class MapSample extends StatefulWidget {
   @override
-  State<RootPage> c
-  createState() => _RootPageState();
+  State<MapSample> createState() => MapSample();
 }
 
-class _RootPageState extends State<RootPage> {
-  @override
-  Widget build(BuildContext contex) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter'),
+class MapSampleState extends State<MapSample> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.4279633588664, -122.885749655962),
+    zoom: 14.4746,
+  );
+  Static final CameraPosition _kLake = CameraPosition(
+    bearing: 192.8334901395799,
+    target: LatLng(37.43296265331129, -122.08832357078792)
+  );
+}
+void _onMapCreated(GoogleMapControlle controller){
+  _controller.complete(controller);
+
+
+@override
+Widget build(BuildContext context){
+  return MaterialApp(
+    home: Scaffold(
+      body: Stack(
+
+        children:<Widget>[
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: _center,
+              zoom: 11.0,
+            ),
+            
+            myLocationButtonEnabled:false
+          ),
+          Container(
+            padding: EdgeInsets.only(top:500, bottom: 50),
+            child:ListView(
+              padding: EdgeInsets.only(left: 40),
+              scrollDirection: Axis.horizontal,
+              children: getTechniciansInArea(),
+            ),
+          ),
+        ],
+      )
+    ),
+  );
+}
+List<Widget>getTechniciansInArea(){
+  return[
+    Container(
+      height: 200,
+      width: 100,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(20))
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+      color: Colors.white,
+    ),
+  ];
+}
+List<Widget> getTechniciansInArea(){
+  return[
+    
+    technicianCard( technician)
+  ];
+}
+Widget technicianCard(Technician technician){
+    return Container(
+      margin: EdgeInsets.all(Radius.circular(20)),
+      width: 180,
+      decoration:BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        color: Colors.white,
+        boxShadow: [
+        new BoxShadow(
+          color:Colors.grey,
+          blurRadius: 20.0
+        ),
+      ],
+        
       ),
+      
     );
+    
   }
-}
+  
+ Container(
+    margin: EdgeInsets.only(right: 20),
+    width: 100,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.all(Radius.circular(20)),
+      color: Colors.white,
+    ),
+ ),
+ Container(
+  margin:EdgeInsets.only(right: 20),
+  width:200,
+  decoration =BoxDecoration(
+    borderRadius:BorderRadius.all(Radius.circular(20)),
+    color: Colors.white, 
+  ),
+),
